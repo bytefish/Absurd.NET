e66bbb67249852c1aade819763aa132a89efe66e
Philipp Wagner <pwag86@googlemail.com>
2025-11-23 21:16:05 +0100

    * Start Work on Sample Project

---
+++ src/AbsurdSdk.Sample/DockerContainers.cs
@@ -0,0 +1,53 @@
+﻿// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Networks;
+using Testcontainers.PostgreSql;
+
+namespace AbsurdSdk.Sample
+{
+    public class DockerContainers
+    {
+        public static INetwork ServicesNetwork = new NetworkBuilder()
+                .WithName("services")
+                .WithDriver(NetworkDriver.Bridge)
+                .Build();
+
+        public static PostgreSqlContainer PostgresContainer = new PostgreSqlBuilder()
+            .WithName("postgres")
+            .WithImage("postgres:18")
+            .WithNetwork(ServicesNetwork)
+            .WithPortBinding(hostPort: 5432, containerPort: 5432)
+            // Mount Postgres Configuration and SQL Scripts 
+            .WithBindMount(Path.Combine(Directory.GetCurrentDirectory(), "Resources/docker/postgres.conf"), "/usr/local/etc/postgres/postgres.conf")
+            .WithBindMount(Path.Combine(Directory.GetCurrentDirectory(), "Resources/sql/absurd.sql"), "/docker-entrypoint-initdb.d/1-absurd.sql")
+            // Set Username and Password
+            .WithEnvironment(new Dictionary<string, string>
+            {
+                    {"POSTGRES_USER", "postgres" },
+                    {"POSTGRES_PASSWORD", "password" },
+            })
+            // Start Postgres with the given postgres.conf.
+            .WithCommand([
+                "postgres",
+                "-c",
+                "config_file=/usr/local/etc/postgres/postgres.conf"
+            ])
+            // Wait until the Port is exposed.
+            .WithWaitStrategy(Wait
+                .ForUnixContainer()
+                .UntilExternalTcpPortIsAvailable(5432))
+            .Build();
+
+        public static async Task StartAllContainersAsync()
+        {
+            await PostgresContainer.StartAsync();
+        }
+
+        public static async Task StopAllContainersAsync()
+        {
+            await PostgresContainer.StopAsync();
+        }
+    }
+}
